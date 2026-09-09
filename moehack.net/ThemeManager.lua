fab4165fff4814751227fad29fb7ef56ec7f6c3e
erotic roleplay mastermind <124532637+mental1illness@users.noreply.github.com>
2026-09-09 19:41:17 +0700

    Update ThemeManager.lua

--- moehack.net/ThemeManager.lua
+++ moehack.net/ThemeManager.lua
@@ -235,7 +235,7 @@ local ThemeManager = {} do
 
 	function ThemeManager:ApplyToTab(tab)
 		assert(self.Library, 'Must set ThemeManager.Library first!')
-		local groupbox = self:CreateGroupBox(tab)
+		local groupbox = self:CreateLeftGroupBox(tab)
 		self:CreateThemeManager(groupbox)
 	end
 
